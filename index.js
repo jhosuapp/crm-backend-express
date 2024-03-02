41b24b4a77d16cab348f839a3c2b535bf0c394f9
jhosuapp <jhosuapvll@gmail.com>
2024-03-02 12:22:36 -0500

    feat: conect mongodb

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+//Express
 import express from 'express';
+//Mongo
+import mongoose from 'mongoose';
+//Routes
 import { router } from './routes/index.js';
+
+//Conect mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/crm', {
+    useNewUrlParser: true
+});
+
 
 //App
 const app = express();
